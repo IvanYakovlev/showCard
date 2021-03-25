2863288506b27f96d9ba478ac1eba5b9a7c1e50b
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2021-03-25 15:49:02 +0700

    v.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,13 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
@@ -46,9 +51,20 @@ public class Main {
         cardsMap.put("0110110001","Q");
         cardsMap.put("0100100101","K");
         cardsMap.put("0101001100","A");
+        List<File> filesInFolder= new ArrayList<>();
+        String pathStr= "C:\\Users\\Иван\\Desktop\\a";
+        try (Stream<Path> paths = Files.walk(Paths.get(pathStr))) {
+            filesInFolder = Files.walk(Paths.get(pathStr))
+                    .filter(Files::isRegularFile)
+                    .map(Path::toFile)
+                    .collect(Collectors.toList());
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        for (File file: filesInFolder) {
 
-        File file = new File("C:\\Users\\Иван\\Downloads\\java_test_task\\20180821_110443.013_0x1FE201D8.png");
-        BufferedImage img = ImageIO.read(file);
+            BufferedImage img = ImageIO.read(file);
     /*    BufferedImage img = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.SCALE_DEFAULT);
         RescaleOp rescaleOp = new RescaleOp(2f, -250, null);
         rescaleOp.filter(image, img);*/
@@ -58,102 +74,134 @@ public class Main {
         g.dispose();*/
 
 
-        Color mycolor1 = new Color(img.getRGB(184, 638));
-        Color mycolor2 = new Color(img.getRGB(177, 645));
-
-        if (!(getColor(mycolor1).equals("white")&&getColor(mycolor2).equals("white"))) {
-            if (getColor(mycolor1).equals("white")) {
-                System.out.println("Черви");
-            } else {
-                if (getColor(mycolor2).equals("white")) {
-                    System.out.println("Крести");
-                } else if (getColor(mycolor2).equals("red")) {
-                    System.out.println("Бубны");
-                } else {
-                    System.out.println("Пики");
-                }
-            }
-        }
-
-        List<BufferedImage> cardImages = new ArrayList<BufferedImage>();
-
-        cardImages.add(img.getSubimage(148, 592, 23, 23));
-        cardImages.add(img.getSubimage(220, 592, 23, 23));
-        cardImages.add(img.getSubimage(291, 592, 23, 23));
-        cardImages.add(img.getSubimage(363, 592, 23, 23));
-        cardImages.add(img.getSubimage(434, 592, 23, 23));
 
 
-
-        String nameCards="";
-        for (int k = 0; k < cardImages.size(); k++) {
-            BufferedImage card = cardImages.get(k);
-
-
-            int[][] arr = new int[card.getHeight()][card.getWidth()];
-            for (int i = 0;i<card.getHeight();i++){
-                for (int j=0;j<card.getWidth();j++)
-                {
-                    if (getColor(new Color(card.getRGB(j,i))).equals("red")||getColor(new Color(card.getRGB(j,i))).equals("black")){
-                        arr[i][j]=1;
-                    } else {
-                        arr[i][j]=0;
-                    }
-                }
-            }
+            List<BufferedImage> cardImages = new ArrayList<BufferedImage>();
+            if (!getColor(new Color(img.getRGB(149, 592))).equals("black"))
+            cardImages.add(img.getSubimage(149, 592, 23, 23));
+            if (!getColor(new Color(img.getRGB(220, 592))).equals("black"))
+            cardImages.add(img.getSubimage(220, 592, 23, 23));
+            if (!getColor(new Color(img.getRGB(292, 592))).equals("black"))
+            cardImages.add(img.getSubimage(292, 592, 23, 23));
+            if (!getColor(new Color(img.getRGB(363, 592))).equals("black"))
+            cardImages.add(img.getSubimage(363, 592, 23, 23));
+            if (!getColor(new Color(img.getRGB(435, 592))).equals("black"))
+            cardImages.add(img.getSubimage(435, 592, 23, 23));
 
 
+            String nameCards = "";
+            for (int k = 0; k < cardImages.size(); k++) {
+                BufferedImage card = cardImages.get(k);
 
-                for (int i = 0;i<23;i++){
-                    for (int j = 0;j<23;j++){
-                        if (i==0||i==22){
-                            arr[i][j]=9;
+
+                int[][] arr = new int[card.getHeight()][card.getWidth()];
+                for (int i = 0; i < card.getHeight(); i++) {
+                    for (int j = 0; j < card.getWidth(); j++) {
+                        if (getColor(new Color(card.getRGB(j, i))).equals("red") || getColor(new Color(card.getRGB(j, i))).equals("black")) {
+                            arr[i][j] = 1;
                         } else {
-                            if (j<22){
-                                if (arr[i][j]==0&&arr[i][j+1]==1){
-                                    arr[i][j+1]=0;
-                                    if (j<21)
-                                        j++;
-                                } else {
-                                    if (arr[i][j]==1&&arr[i][j+1]==0) {
-                                        arr[i][j] = 0;
-                                        if (j < 21)
-                                            j++;
-                                    }
-                                }
-                            }
+                            arr[i][j] = 0;
                         }
                     }
                 }
 
+                Color suitPoint1 ;
+                Color suitPoint2 ;
 
-            nameCards+=equalsArray(arr)+",";
+                switch (k){
+                    case 0:{
+                        suitPoint1 = new Color(img.getRGB(184, 638));
+                        suitPoint2 = new Color(img.getRGB(175, 644));
+                    }
+                    break;
+                    case 1:{
+                        suitPoint1 = new Color(img.getRGB(256, 638));
+                        suitPoint2 = new Color(img.getRGB(247, 644));
+                    }
+                    break;
+                    case 2:{
+                        suitPoint1 = new Color(img.getRGB(327, 638));
+                        suitPoint2 = new Color(img.getRGB(318, 644));
+                    }
+                    break;
+                    case 3:{
+                        suitPoint1 = new Color(img.getRGB(399, 638));
+                        suitPoint2 = new Color(img.getRGB(390, 644));
+                    }
+                    break;
+                    case 4:{
+                        suitPoint1 = new Color(img.getRGB(471, 638));
+                        suitPoint2 = new Color(img.getRGB(462, 644));
+                    }
+                    break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + k);
+                }
 
+                String suit="";
+
+                if (!(getColor(suitPoint1).equals("white") && getColor(suitPoint2).equals("white"))) {
+                    if (getColor(suitPoint1).equals("white")) {
+                        suit="h";
+                    } else {
+                        if (getColor(suitPoint2).equals("white")) {
+                            suit="c";
+                        } else if (getColor(suitPoint2).equals("red")) {
+                            suit="d";
+                        } else {
+                            suit="s";
+                        }
+                    }
+                }
+
+                nameCards += equalsArray(arr) + suit+"";
+
+            }
+
+        /*    System.out.println(file.getName()+" - "+nameCards);
+            JFrame myJFrame = new JFrame("Image pane");
+            myJFrame.setSize(200, 200);
+            myJFrame.setVisible(true);
+            myJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            myJFrame.setContentPane(new ImagePanel(cardImages.get(0), cardImages.get(1), cardImages.get(2), cardImages.get(2), cardImages.get(2)));
+*/
         }
-
-        System.out.println(nameCards);
-        JFrame myJFrame = new JFrame("Image pane");
-        myJFrame.setSize(200,200);
-        myJFrame.setVisible(true);
-        myJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        myJFrame.setContentPane(new ImagePanel(cardImages.get(0),cardImages.get(1),cardImages.get(2),cardImages.get(3),cardImages.get(4)));
-
-
     }
     public static String equalsArray(int[][] b){
 
-        for (int i = 0; i < 23; i++) {
+ /*       for (int i = 0; i < 23; i++) {
             for (int j = 0; j < 23; j++) {
-                System.out.print(b[i][j]+" ");
+                System.out.print(b[i][j]+", ");
             }
             System.out.println("");
 
         }
-        System.out.println("//////////////////////////////////////////////////");
+        System.out.println("//////////////////////////////////////////////////");*/
 
-        String currentValue =""+ b[x1][y1]+b[x2][y2]+b[x3][y3]+b[x4][y4]+b[x5][y5]+b[x6][y6]+b[x7][y7]+b[x8][y8]+b[x9][y9]+b[x10][y10];
+        String str="NULL";
+        int errors= 0;
+        double err=0;
+        Map<int[][],String> map = Templates.getList();
+        List<int[][]> keys = new ArrayList<int[][]>(map.keySet());
+        for (int l=0;l<keys.size();l++){
+            int[][]currArr = keys.get(l);
+            for (int i = 0; i < 23; i++) {
+                for (int j = 0; j < 23; j++) {
+                    if (currArr[i][j]!=b[i][j])
+                        errors++;
+                        err+=(currArr[i][j]-b[i][j])^2;
+                }
+            }
+            String CARD = map.get(currArr);
+            if (errors<60){
+                str=map.get(currArr);
+                break;
 
-        return cardsMap.get(currentValue);
+            }
+            errors=0;
+        }
+
+        return str;
 
     }
     public static String getColor(Color color){
