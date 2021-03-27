a7f077bf57d72c6871edb83e8387232fa63f115c
Ivan Yakovlev <ivanyakovlev.jd@gmail.com>
2021-03-27 19:45:59 +0700

    v.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,63 +2,75 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ClassLoader classLoader = Main.class.getClassLoader();
         List<File> filesInFolder= new ArrayList<>();
-        String pathStr= "C:\\Users\\Иван\\Desktop\\a";
+        String pathStr= args[0];
+        Properties prop = new Properties();
+
         try (Stream<Path> paths = Files.walk(Paths.get(pathStr))) {
             filesInFolder = paths.filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
+            prop.load(classLoader.getResourceAsStream("config.properties"));
         }
         catch (Exception e){
             System.out.println(e);
         }
 
-        Map<int[][], String> cardMap= new HashMap<>();
-        ClassLoader classLoader = Main.class.getClassLoader();
-        File cardFile = new File(classLoader.getResource("card.txt").getFile());
+        int cardHeight=Integer.parseInt((String) prop.get("cardHeight"));
+        int cardWidth=Integer.parseInt((String) prop.get("cardWidth"));
+        int suitHeight=Integer.parseInt((String) prop.get("suitHeight"));
+        int suitWidth=Integer.parseInt((String) prop.get("suitWidth"));
 
-        int[][] cardArray = new int[20][20];
-        try (BufferedReader reader = new BufferedReader(new FileReader(cardFile)))
-        {
-          String line = reader.readLine();
-          int i=0;
-          while (line!=null){
-              String[] arrStr = line.split(" ");
+        File cardFile = new File(classLoader.getResource("templates/card.txt").getFile());
+        File suitFile = new File(classLoader.getResource("templates/suit.txt").getFile());
 
-              if (arrStr.length>1){
-                  int[] arr =  Arrays.asList(arrStr).stream().mapToInt(Integer::parseInt).toArray();
-                  cardArray[i]=arr;
-                  i++;
-              } else {
-                  cardMap.put(cardArray,arrStr[0]);
-                  cardArray=new int[20][20];
-                  i=0;
-              }
+        Map<int[][], String> cardTemplateMap = getTemplateMap(cardFile, cardHeight, cardWidth);
+        Map<int[][], String> suitTemplateMap = getTemplateMap(suitFile, suitHeight, suitWidth);
 
-              line=reader.readLine();
-          }
+        for (File file: filesInFolder) {
+            System.out.println(getCards(file,cardTemplateMap ,suitTemplateMap, cardHeight, cardWidth, suitHeight, suitWidth));
+        }
+    }
+    //Метод читает из файла шаблона данные и преобразует их в Map
+    public static Map<int[][], String> getTemplateMap(File cardFile, int h, int w) {
+
+        Map<int[][], String> cardTemplateMap = new HashMap<>();
+        int[][] cardArray = new int[h][w];
+        try (BufferedReader reader = new BufferedReader(new FileReader(cardFile))){
+
+            String line = reader.readLine();
+            int i=0;
+            while (line!=null){
+                String[] arrStr = line.split(" ");
+
+                if (arrStr.length>1){
+                    int[] arr =  Arrays.asList(arrStr).stream().mapToInt(Integer::parseInt).toArray();
+                    cardArray[i]=arr;
+                    i++;
+                } else {
+                    cardTemplateMap.put(cardArray,arrStr[0]);
+                    cardArray=new int[h][w];
+                    i=0;
+                }
+                line=reader.readLine();
+            }
         }
         catch (IOException | NumberFormatException e)
         {
             e.printStackTrace();
         }
-
-        for (File file: filesInFolder) {
-            System.out.println(getCards(file,cardMap));
-        }
+        return cardTemplateMap;
     }
-
-    public static String getCards(File file, Map<int[][],String> cardMap) {
+    //Метод возвращает достоинства и масти карт из переданного файла
+    public static String getCards(File file, Map<int[][],String> cardTemplate, Map<int[][],String> suitTemplate, int cardHeight,int cardWidth,int suitHeight,int suitWidth) {
 
         BufferedImage img = null;
         try {
@@ -69,103 +81,45 @@ public class Main {
 
         List<BufferedImage> cardImages = new ArrayList<BufferedImage>();
         List<BufferedImage> suitImages = new ArrayList<BufferedImage>();
-        if (getColor(new Color(img.getRGB(151, 588))).equals("white"))
-            cardImages.add(img.getSubimage(151, 593, 20, 20));
-            suitImages.add(img.getSubimage(176, 614, 15, 15));
+        if (getColor(new Color(img.getRGB(151, 588))).equals("white")) //если в указанной точке белый фон то перед нами карта
+            cardImages.add(img.getSubimage(151, 593, cardWidth, cardHeight)); // задаем координаты достоинства карты
+            suitImages.add(img.getSubimage(169, 641, suitWidth, suitHeight)); // задаем координаты масьи карты
         if (getColor(new Color(img.getRGB(223, 588))).equals("white"))
-            cardImages.add(img.getSubimage(223, 593, 20, 20));
-            suitImages.add(img.getSubimage(248, 614, 15, 15));
+            cardImages.add(img.getSubimage(223, 593, cardWidth, cardHeight));
+            suitImages.add(img.getSubimage(241, 641, suitWidth, suitHeight));
         if (getColor(new Color(img.getRGB(294, 588))).equals("white"))
-            cardImages.add(img.getSubimage(294, 593, 20, 20));
-            suitImages.add(img.getSubimage(319, 614, 15, 15));
+            cardImages.add(img.getSubimage(294, 593, cardWidth, cardHeight));
+            suitImages.add(img.getSubimage(312, 641, suitWidth, suitHeight));
         if (getColor(new Color(img.getRGB(366, 588))).equals("white"))
-            cardImages.add(img.getSubimage(366, 593, 20, 20));
-            suitImages.add(img.getSubimage(391, 614, 15, 15));
+            cardImages.add(img.getSubimage(366, 593, cardWidth, cardHeight));
+            suitImages.add(img.getSubimage(384, 641, suitWidth, suitHeight));
         if (getColor(new Color(img.getRGB(438, 588))).equals("white"))
-            cardImages.add(img.getSubimage(438, 593, 20, 20));
-            suitImages.add(img.getSubimage(463, 614, 15, 15));
+            cardImages.add(img.getSubimage(438, 593, cardWidth, cardHeight));
+            suitImages.add(img.getSubimage(456, 641, suitWidth, suitHeight));
 
         String nameCards = "";
         for (int k = 0; k < cardImages.size(); k++) {
             BufferedImage card = cardImages.get(k);
+            BufferedImage suit = suitImages.get(k);
 
-            int[][] cardArr = new int[card.getHeight()][card.getWidth()];
-            for (int i = 0; i < card.getHeight(); i++) {
-                for (int j = 0; j < card.getWidth(); j++) {
-                    if (getColor(new Color(card.getRGB(j, i))).equals("black")||getColor(new Color(card.getRGB(j, i))).equals("red")) {
-                        cardArr[i][j] = 1;
-                    } else {
-                        cardArr[i][j] = 0;
-                    }
-                }
-            }
-
-            Color suitPoint1 ;
-            Color suitPoint2 ;
-
-            switch (k){
-                case 0:{
-                    suitPoint1 = new Color(img.getRGB(184, 638));
-                    suitPoint2 = new Color(img.getRGB(176, 645));
-                }
-                break;
-                case 1:{
-                    suitPoint1 = new Color(img.getRGB(256, 638));
-                    suitPoint2 = new Color(img.getRGB(248, 644));
-                }
-                break;
-                case 2:{
-                    suitPoint1 = new Color(img.getRGB(327, 638));
-                    suitPoint2 = new Color(img.getRGB(319, 644));
-                }
-                break;
-                case 3:{
-                    suitPoint1 = new Color(img.getRGB(399, 638));
-                    suitPoint2 = new Color(img.getRGB(391, 644));
-                }
-                break;
-                case 4:{
-                    suitPoint1 = new Color(img.getRGB(471, 638));
-                    suitPoint2 = new Color(img.getRGB(463, 644));
-                }
-                break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + k);
-            }
-
-            String suit="";
-
-            if (!(getColor(suitPoint1).equals("white") && getColor(suitPoint2).equals("white"))) {
-                if (getColor(suitPoint1).equals("white")) {
-                    suit="h";
-                } else {
-                    if (getColor(suitPoint2).equals("white")) {
-                        suit="c";
-                    } else if (getColor(suitPoint2).equals("red")) {
-                        suit="d";
-                    } else {
-                        suit="s";
-                    }
-                }
-            }
-
-            nameCards += equalsArray(cardArr,cardMap) + suit+"";
-
+            nameCards += recognize(card,cardTemplate) + recognize(suit, suitTemplate);
         }
 
         return file.getName()+" - "+nameCards;
     }
-
-    public static String equalsArray(int[][] b, Map<int[][], String> map){
-
-/*        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                System.out.print(b[i][j]+", ");
+//Метод возвращает наиболее подходящее из шаблона значение
+    private static String recognize(BufferedImage img, Map<int[][], String> map) {
+        //преобразуем картинку в двумерный массив где 0 - фон 1 - значимый пиксель
+        int[][] arr = new int[img.getHeight()][img.getWidth()];
+        for (int i = 0; i < img.getHeight(); i++) {
+            for (int j = 0; j < img.getWidth(); j++) {
+                if (getColor(new Color(img.getRGB(j, i))).equals("black")||getColor(new Color(img.getRGB(j, i))).equals("red")) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = 0;
+                }
             }
-            System.out.println("");
-
         }
-        System.out.println("//////////////////////////////////////////////////");*/
 
         String str="NULL";
         int error= 0;
@@ -174,24 +128,26 @@ public class Main {
 
         for (int l=0;l<keys.size();l++){
             int[][]currArr = keys.get(l);
-            for (int i = 0; i < 20; i++) {
-                for (int j = 0; j < 20; j++) {
-                    if (currArr[i][j]!=b[i][j])
-                        error++;
+            for (int i = 0; i < img.getHeight(); i++) {
+                for (int j = 0; j < img.getWidth(); j++) {
+                    //System.out.print(arr[i][j]+" ");  Использовалось для печати шаблонов
+                    if (currArr[i][j]!=arr[i][j])
+                        error++; // складываем ошибки полученные при сравнении массива из шаблона и картинки
                 }
+                //System.out.println("");  Использовалось для печати шаблонов
             }
-            String CARD = map.get(currArr);
-            mapErrors.put(error,map.get(currArr));
+            mapErrors.put(error,map.get(currArr));//кладем error в файл напротив значения из шаблона
 
             error=0;
         }
-        str= mapErrors.get(Collections.min(mapErrors.keySet()));
+        str= mapErrors.get(Collections.min(mapErrors.keySet())); // Возвращаем значение из map с ошибками с наименьшим количеством ошибок
         return str;
-
     }
+//метод возвращает цвет пикселя
     public static String getColor(Color color){
-        if ((color.getRed() == 255 && color.getGreen() == 255 && color.getBlue() == 255)||
-                color.getRed() == 120 && color.getGreen() == 120 && color.getBlue() == 120) return "white";
+
+        if ((color.getRed() == 255 && color.getGreen() == 255 && color.getBlue() == 255)|| // если карта активна
+                color.getRed() == 120 && color.getGreen() == 120 && color.getBlue() == 120) return "white"; // или если карта затемнена
         if ((color.getRed() == 205 && color.getGreen() == 73 && color.getBlue() == 73)||
                 (color.getRed() == 96 && color.getGreen() == 34 && color.getBlue() == 34)) return "red";
         if ((color.getRed() == 16 && color.getGreen() == 16 && color.getBlue() == 18)||
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,92 +1,87 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 class MainTest {
 
     @Test
     void cardsAll() {
-        Map<int[][], String> cardMap= new HashMap<>();
-
         ClassLoader classLoader = Main.class.getClassLoader();
-        File cardFile = new File(classLoader.getResource("card.txt").getFile());
+        File cardFile = new File(classLoader.getResource("templates/card.txt").getFile());
+        File suitFile = new File(classLoader.getResource("templates/suit.txt").getFile());
+        Map<int[][], String> cardTemplateMap = Main.getTemplateMap(cardFile, 20,20);
+        Map<int[][], String> suitTemplateMap = Main.getTemplateMap(suitFile, 24,30);
 
-
-        int[][] cardArray = new int[20][20];
-        try (BufferedReader reader = new BufferedReader(new FileReader(cardFile)))
-        {
-            String line = reader.readLine();
-            int i=0;
-            while (line!=null){
-                String[] arrStr = line.split(" ");
-
-                if (arrStr.length>1){
-                    int[] arr = new int[20];
-                    arr = Arrays.asList(arrStr).stream().mapToInt(Integer::parseInt).toArray();
-                    cardArray[i]=arr;
-                    i++;
-                } else {
-                    cardMap.put(cardArray,arrStr[0]);
-                    cardArray=new int[20][20];
-                    i=0;
-                }
-
-                line=reader.readLine();
-            }
-        }
-        catch (IOException | NumberFormatException e)
-        {
+        Properties prop = new Properties();
+        try {
+            prop.load(classLoader.getResourceAsStream("config.properties"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        String val1= Main.getCards(new File("src/test/resources/20180821_064009.827_0x1934025E.png"),cardMap);
+        int cardHeight=Integer.parseInt((String) prop.get("cardHeight"));
+        int cardWidth=Integer.parseInt((String) prop.get("cardWidth"));
+        int suitHeight=Integer.parseInt((String) prop.get("suitHeight"));
+        int suitWidth=Integer.parseInt((String) prop.get("suitWidth"));
+
+        String val1= Main.getCards(new File("src/test/resources/20180821_064009.827_0x1934025E.png"),
+                cardTemplateMap, suitTemplateMap, cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_064009.827_0x1934025E.png", val1.equals("20180821_064009.827_0x1934025E.png - JsAh10sQsKc"));
 
-        String val2= Main.getCards(new File("src/test/resources/20180821_084134.865_0x240C023E.png"),cardMap);
-        Assert.assertTrue("20180821_084134.865_0x240C023E.png", val2.equals("20180821_084134.865_0x240C023E.png - Kh3s2s10c"));
+        String val2= Main.getCards(new File("src/test/resources/20180821_084134.865_0x240C023E.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_084134.865_0x240C023E.png", val2.equals("20180821_084134.865_0x240C023E.png - Kh3d2d10c"));
 
-        String val3= Main.getCards(new File("src/test/resources/20180821_110443.013_0x1FE201D8.png"),cardMap);
+        String val3= Main.getCards(new File("src/test/resources/20180821_110443.013_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_110443.013_0x1FE201D8.png", val3.equals("20180821_110443.013_0x1FE201D8.png - 6cKc2c8s8h"));
 
-        String val4= Main.getCards(new File("src/test/resources/20180821_113501.953_0x1CFF023A.png"),cardMap);
-        Assert.assertTrue("20180821_113501.953_0x1CFF023A.png", val4.equals("20180821_113501.953_0x1CFF023A.png - 3h9h5s"));
+        String val4= Main.getCards(new File("src/test/resources/20180821_113501.953_0x1CFF023A.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_113501.953_0x1CFF023A.png", val4.equals("20180821_113501.953_0x1CFF023A.png - 3h9h5d"));
 
-        String val5= Main.getCards(new File("src/test/resources/20180821_120619.905_0x1FE201D8.png"),cardMap);
+        String val5= Main.getCards(new File("src/test/resources/20180821_120619.905_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_120619.905_0x1FE201D8.png", val5.equals("20180821_120619.905_0x1FE201D8.png - 4s4h7hKc"));
 
-        String val6= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093622.029_0x0EDA02B0.png"),cardMap);
+        String val6= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093622.029_0x0EDA02B0.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_093622.029_0x0EDA02B0.png", val6.equals("20180821_093622.029_0x0EDA02B0.png - 10c3s9s"));
 
-        String val7= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093714.230_0x1FE201D8.png"),cardMap);
+        String val7= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093714.230_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_093714.230_0x1FE201D8.png", val7.equals("20180821_093714.230_0x1FE201D8.png - Qs10h9s"));
 
-        String val8= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093736.244_0x1FE201D8.png"),cardMap);
-        Assert.assertTrue("20180821_093736.244_0x1FE201D8.png", val8.equals("20180821_093736.244_0x1FE201D8.png - 10sAc10s"));
+        String val8= Main.getCards(new File("src/test/resources/kingAceTen/20180821_093736.244_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_093736.244_0x1FE201D8.png", val8.equals("20180821_093736.244_0x1FE201D8.png - 10sAc10d"));
 
-        String val9= Main.getCards(new File("src/test/resources/kingAceTen/20180821_094236.839_0x1FE201D8.png"),cardMap);
-        Assert.assertTrue("20180821_094236.839_0x1FE201D8.png", val9.equals("20180821_094236.839_0x1FE201D8.png - AhAsQc"));
+        String val9= Main.getCards(new File("src/test/resources/kingAceTen/20180821_094236.839_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_094236.839_0x1FE201D8.png", val9.equals("20180821_094236.839_0x1FE201D8.png - AhAdQc"));
 
-        String val10= Main.getCards(new File("src/test/resources/kingAceTen/20180821_094732.846_0x1FE201D8.png"),cardMap);
-        Assert.assertTrue("20180821_094732.846_0x1FE201D8.png", val10.equals("20180821_094732.846_0x1FE201D8.png - Jd10sAc"));
+        String val10= Main.getCards(new File("src/test/resources/kingAceTen/20180821_094732.846_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_094732.846_0x1FE201D8.png", val10.equals("20180821_094732.846_0x1FE201D8.png - Jd10dAc"));
 
-        String val11= Main.getCards(new File("src/test/resources/kingAceTen/20180821_095616.438_0x1FE201D8.png"),cardMap);
+        String val11= Main.getCards(new File("src/test/resources/kingAceTen/20180821_095616.438_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_095616.438_0x1FE201D8.png", val11.equals("20180821_095616.438_0x1FE201D8.png - 5sKcKs"));
 
-        String val12= Main.getCards(new File("src/test/resources/kingAceTen/20180821_095755.396_0x0EDA02B0.png"),cardMap);
+        String val12= Main.getCards(new File("src/test/resources/kingAceTen/20180821_095755.396_0x0EDA02B0.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
         Assert.assertTrue("20180821_095755.396_0x0EDA02B0.png", val12.equals("20180821_095755.396_0x0EDA02B0.png - 5dKhAs"));
 
-        String val13= Main.getCards(new File("src/test/resources/kingAceTen/20180821_110027.430_0x1FE201D8.png"),cardMap);
-        Assert.assertTrue("20180821_110027.430_0x1FE201D8.png", val13.equals("20180821_110027.430_0x1FE201D8.png - 8s10h8sAs"));
+        String val13= Main.getCards(new File("src/test/resources/kingAceTen/20180821_110027.430_0x1FE201D8.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_110027.430_0x1FE201D8.png", val13.equals("20180821_110027.430_0x1FE201D8.png - 8s10h8dAs"));
 
-        String val14= Main.getCards(new File("src/test/resources/kingAceTen/20180821_120241.032_0x1CFF023A.png"),cardMap);
-        Assert.assertTrue("20180821_120241.032_0x1CFF023A.png", val14.equals("20180821_120241.032_0x1CFF023A.png - 4sJsKsKc"));
+        String val14= Main.getCards(new File("src/test/resources/kingAceTen/20180821_120241.032_0x1CFF023A.png"),
+                cardTemplateMap, suitTemplateMap,cardHeight,cardWidth,suitHeight,suitWidth);
+        Assert.assertTrue("20180821_120241.032_0x1CFF023A.png", val14.equals("20180821_120241.032_0x1CFF023A.png - 4sJdKdKc"));
     }
 
 }
